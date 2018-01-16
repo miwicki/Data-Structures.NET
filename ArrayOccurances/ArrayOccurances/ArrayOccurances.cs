5b5758a5191dbe3d7d3f55aee2ef0a511e2b07a0
Matt <wrestler.411@comcast.net>
2018-01-16 02:02:46 -0800

    Finished array occurences

---
+++ ArrayOccurances/ArrayOccurances/ArrayOccurances.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace ArrayOccurences
+{
+    class OccurancesCount
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = { 1, 4, 4, 4, 4, 5, 6, 6, 6, 9, 9, 12, 1, 1, 4 };
+            OccurancesAllEdgeCases(numArray);
+            Console.ReadLine();
+        }
+
+        static void PrintOccurances(int[] input)
+        {
+            int counter = 1;
+            int prev, curr;
+            prev = input[0];
+            curr = input[1];
+
+            int index = 1;
+            while (index < input.Length -1)
+            {
+                if (curr != prev)
+                {
+                    Console.WriteLine($"{prev}:{counter} occurances");
+                    counter = 0;
+                }
+                counter++;
+                prev = curr;
+                curr = input[++index];
+            }
+            Console.WriteLine($"{prev}:{++counter} occurances");
+        }
+
+        
+    }
+}
