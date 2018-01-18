65afbdd3b565887997e1da37bbd328ac12554283
Matt <wrestler.411@comcast.net>
2018-01-17 16:51:39 -0800

    code is working but breadthfirst has incorrect output

--- Trees/Trees/Program.cs
+++ Trees/Trees/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Trees
 {
     //creating node class up here
     class Node
     {
-        
+
         public Node left;
         public Node right;
         private char v;
@@ -15,36 +17,44 @@ namespace Trees
             this.v = v;
         }
 
-       
 
-    //making actual tree and running tests down here
-    class Program
-    {
-        static void Main(string[] args)
+
+        //making actual tree and running tests down here
+        class Program
         {
+            static void Main(string[] args)
+            {
 
-            Node a = new Node('A');
-            Node b = new Node('B');
-            Node c = new Node('C');
-            Node d = new Node('D');
-            Node e = new Node('E');
+                Node a = new Node('A');
+                Node b = new Node('B');
+                Node c = new Node('C');
+                Node d = new Node('D');
+                Node e = new Node('E');
+                Node f = new Node('F');
+                Node g = new Node('G');
 
-            a.left = b;
-            a.right = c;
-            b.left = d;
-            b.right = e;
+                a.left = b;
+                a.right = c;
+                b.left = d;
+                b.right = e;
+                c.left = f;
+                c.right = g;
 
-            Console.WriteLine("PreOrder");
-            Preorder(a);
-            Console.WriteLine("---------");
-            Console.WriteLine("InOrder");
-            Inorder(a);
-            Console.WriteLine("---------");
-            Console.WriteLine("PostOrder");
-            Postorder(a);
-            Console.WriteLine("---------");
+                Console.WriteLine("PreOrder");
+                Preorder(a);
+                Console.WriteLine("---------");
+                Console.WriteLine("InOrder");
+                Inorder(a);
+                Console.WriteLine("---------");
+                Console.WriteLine("PostOrder");
+                Postorder(a);
+                Console.WriteLine("---------");
+                Console.WriteLine("BreadthFirst");
+                BreadthFirst(a);
+                Console.WriteLine("---------");
                 Console.ReadLine();
-        }
+
+            }
 
             //Putting methods here so tests can read them
             public static void Preorder(Node Root)
@@ -74,7 +84,21 @@ namespace Trees
                     Console.Write(Root.v + " ");
                 }
             }
+
+            public static void BreadthFirst(Node Root)
+            {
+
+                Queue<Node> q = new Queue<Node>();
+                Console.WriteLine(Root.v);
+                if (Root.left != null) q.Enqueue(Root.left);
+                if (Root.right != null) q.Enqueue(Root.right);
+
+                if (q.Count > 0)
+                {
+                    BreadthFirst(q.Dequeue());
+                }
+            }
         }
     }
 }
-   
+
