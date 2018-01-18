e9830590ced0b21300e5989907ef408d763b6906
Matt <wrestler.411@comcast.net>
2018-01-17 17:46:05 -0800

    Had to put BreadthFirst method into a whole new Tree class to make it work; looks disorganized but it works

--- Trees/Trees/Program.cs
+++ Trees/Trees/Program.cs
@@ -10,86 +10,23 @@ namespace Trees
 
         public Node left;
         public Node right;
-        private char v;
+        public char v;
 
         public Node(char v)
         {
             this.v = v;
         }
+    }
 
-
-
-        //making actual tree and running tests down here
-        class Program
+    class Tree
+    {
+        Queue<Node> q = new Queue<Node>();
+        public void BreadthFirst(Node Root)
         {
-            static void Main(string[] args)
+
+            if (Root != null)
             {
-
-                Node a = new Node('A');
-                Node b = new Node('B');
-                Node c = new Node('C');
-                Node d = new Node('D');
-                Node e = new Node('E');
-                Node f = new Node('F');
-                Node g = new Node('G');
-
-                a.left = b;
-                a.right = c;
-                b.left = d;
-                b.right = e;
-                c.left = f;
-                c.right = g;
-
-                Console.WriteLine("PreOrder");
-                Preorder(a);
-                Console.WriteLine("---------");
-                Console.WriteLine("InOrder");
-                Inorder(a);
-                Console.WriteLine("---------");
-                Console.WriteLine("PostOrder");
-                Postorder(a);
-                Console.WriteLine("---------");
-                Console.WriteLine("BreadthFirst");
-                BreadthFirst(a);
-                Console.WriteLine("---------");
-                Console.ReadLine();
-
-            }
-
-            //Putting methods here so tests can read them
-            public static void Preorder(Node Root)
-            {
-                if (Root != null)
-                {
-                    Console.Write(Root.v + " ");
-                    Preorder(Root.left);
-                    Preorder(Root.right);
-                }
-            }
-            public static void Inorder(Node Root)
-            {
-                if (Root != null)
-                {
-                    Inorder(Root.left);
-                    Console.Write(Root.v + " ");
-                    Inorder(Root.right);
-                }
-            }
-            public static void Postorder(Node Root)
-            {
-                if (Root != null)
-                {
-                    Postorder(Root.left);
-                    Postorder(Root.right);
-                    Console.Write(Root.v + " ");
-                }
-            }
-
-            public static void BreadthFirst(Node Root)
-            {
-
-                Queue<Node> q = new Queue<Node>();
-                Console.WriteLine(Root.v);
+                Console.Write(Root.v + " ");
                 if (Root.left != null) q.Enqueue(Root.left);
                 if (Root.right != null) q.Enqueue(Root.right);
 
@@ -99,6 +36,87 @@ namespace Trees
                 }
             }
         }
+
     }
+
+    //making actual tree and running tests down here
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Node a = new Node('A');
+            Node b = new Node('B');
+            Node c = new Node('C');
+            Node d = new Node('D');
+            Node e = new Node('E');
+            Node f = new Node('F');
+            Node g = new Node('G');
+
+            a.left = b;
+            a.right = c;
+            b.left = d;
+            b.right = e;
+            c.left = f;
+            c.right = g;
+
+
+            //Preorder Expected: A B D E C F G
+            Console.WriteLine("PreOrder");
+            Preorder(a);
+            Console.WriteLine("");
+
+            //InOrder Expected: D B E A F C G
+            Console.WriteLine("InOrder");
+            Inorder(a);
+            Console.WriteLine("");
+
+            //PostOreder Expected: D E B F G C A
+            Console.WriteLine("PostOrder");
+            Postorder(a);
+            Console.WriteLine("");
+
+            //BreadthFirst Expected: A B C D E F G
+            Console.WriteLine("BreadthFirst");
+            Tree myTree = new Tree();
+            myTree.BreadthFirst(a);
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+
+        //Putting methods here so tests can read them
+        public static void Preorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Console.Write(Root.v + " ");
+                Preorder(Root.left);
+                Preorder(Root.right);
+            }
+        }
+        public static void Inorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Inorder(Root.left);
+                Console.Write(Root.v + " ");
+                Inorder(Root.right);
+            }
+        }
+        public static void Postorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Postorder(Root.left);
+                Postorder(Root.right);
+                Console.Write(Root.v + " ");
+            }
+        }
+
+
+    }
+
 }
 
